2ea549765252790cc0bdb26cd1c740e5f100aba5
Joel Bateman <batemanjoel4@gmail.com>
2015-03-12 19:56:24 -0700

    Chapter 1 complete - Still new to do some sizing configuration for the iOS simulator on this project

---
+++ Chapter 1/Quiz/Quiz/BNRViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRViewController.h
+//  Quiz
+//
+//  Created by Joel Bateman on 3/4/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRViewController : UIViewController
+
+@end
---
+++ Chapter 1/Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Joel Bateman on 3/4/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
