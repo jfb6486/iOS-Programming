07d091deb1715fa20a4a9edee654abbb1d8d0905
Joel Bateman <batemanjoel4@gmail.com>
2015-03-17 10:13:40 -0700

    Pushing up Chapter 2 progress

---
+++ Chapter 2/RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Joel Bateman on 3/13/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ Chapter 2/RandomItems/RandomItems/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Joel Bateman on 3/13/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
