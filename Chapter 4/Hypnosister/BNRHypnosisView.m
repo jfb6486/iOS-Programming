711edecca4a7817ccb70f678a1a68ff0b97d1002
Joel Bateman <batemanjoel4@gmail.com>
2015-03-24 21:55:32 -0700

    Chapter 4 progress

---
+++ Chapter 4/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Joel Bateman on 3/24/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+@end
---
+++ Chapter 4/Hypnosister/Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Joel Bateman on 3/23/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
