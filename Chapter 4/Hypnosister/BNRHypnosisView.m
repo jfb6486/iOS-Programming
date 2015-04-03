d6f5b94b19e8185ec47efa69c3a2f72ce7fd8b95
Joel Bateman <batemanjoel4@gmail.com>
2015-04-02 22:49:17 -0700

    Chapter 4 complete (Custom drawing in drawRect, UIBezierPath intro, Drawing concentric circles)

--- Chapter 4/Hypnosister/BNRHypnosisView.m
+++ Chapter 4/Hypnosister/BNRHypnosisView.m
@@ -10,6 +10,18 @@
 
 @implementation BNRHypnosisView
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if(self){
+        // All BNRHyponsisViews start with a clear background
+        self.backgroundColor = [UIColor clearColor];
+    }
+    
+    return self;
+}
+
 - (void)drawRect:(CGRect)rect
 {
     CGRect bounds = self.bounds;
@@ -19,18 +31,30 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    // The circle that will the largest that will fit in the view
-    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    // The largest circle will circumscribe the view
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     UIBezierPath *path = [[UIBezierPath alloc]init];
     
-    // Add an arc to the path at the center with radius of radius,
-    // from 0 to 2*PI radians (a circle)
-    [path addArcWithCenter:center
-                    radius:radius
-                    startAngle:0.0
-                    endAngle:M_PI * 2.0
-                    clockwise:YES];
+    
+    for(float currentRadius=maxRadius;currentRadius>0;currentRadius-=20){
+        
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+
+        
+        [path addArcWithCenter:center
+                        radius:currentRadius
+         // Note this is not the current radius!
+                        startAngle:0.0
+                        endAngle:M_PI*2.0
+                        clockwise:YES];
+    }
+    
+    // Configure the line width to 10 points
+    path.lineWidth=10;
+    
+    // Configure the drawing color to light grey
+    [[UIColor lightGrayColor] setStroke];
     
     // Draw the line!
     [path stroke];
