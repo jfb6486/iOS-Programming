033c13cce1d2d34f8abdcb0b0d50f0ba336d6cd4
Joel Bateman <batemanjoel4@gmail.com>
2015-04-05 16:15:38 -0700

    Chapter 6 started

---
+++ Chapter 6/HypnoNerd/HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Joel Bateman on 4/5/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : NSObject
+
+@end
---
+++ Chapter 6/HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by Joel Bateman on 4/5/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end
